05a362f7a06e144150084d2a02887b90b9bc3082
RosePoreaux <62560301+RosePoreaux@users.noreply.github.com>
2020-05-12 16:33:23 +0200

    Update tab_about.py

--- pseudo_app/components/tab_about.py
+++ pseudo_app/components/tab_about.py
@@ -15,30 +15,28 @@ pour une République numérique fait de l’ouverture des données publiques la 
 l'occultation préalable des éléments à caractère personnel est [une obligation légale](https://www.legifrance.gouv.fr/affichCodeArticle.do?idArticle=LEGIARTI000033205514&cidTexte=LEGITEXT000031366350&dateTexte=20161009)
 qui s’impose aux administrations.
 Afin de faciliter l'ouverture au grand public, une solution automatisée de pseudonymisation, comme celle-ci, est donc
-souhaitable pour faire face à la grande quantité des documents à traiter dans un temps raisonnable. 
+souhaitable pour faire face à la grande quantité de documents à traiter dans un temps raisonnable. 
 
 Cette démo, [l'API](https://github.com/psorianom/pseudo_api), et la [guide pseudonymisation](https://guides.etalab.gouv.fr/pseudonymisation/)
-(où ous pouvez trouver beaucoup plus d'informations) font partie des outils 
- mutuaisables du [Lab IA](https://www.etalab.gouv.fr/datasciences-et-intelligence-artificielle)
+(où vous pouvez trouver beaucoup plus d'informations) font partie des outils 
+ mutualisés du [Lab IA](https://www.etalab.gouv.fr/datasciences-et-intelligence-artificielle)
 [d'Etalab](https://www.etalab.gouv.fr).
 
 """)
 
 COMMENT = dcc.Markdown("""
-La pseudonymisation peut être vu comme un problème de traitement de langage naturel, plus particulièrement,
-comme une tache de [reconnaissance d'entités nommées](https://fr.wikipedia.org/wiki/Reconnaissance_d%27entit%C3%A9s_nomm%C3%A9es)
-(REN). La REN est souvent résolue à l'aide des méthodes d'apprentissage supervisé séquentiel. Cette démo utilise une approche de ce type,
-c'est-à-dire, nous avons entraîné un modèle en utilisant des décisions de justice annotées indirectement. Le modèle est
-ensuite normalement capable de repérer des entités (noms, prènoms, adresses) dans des nouveaux documents. 
+La pseudonymisation peut être effectuée grâce au traitement du langage naturel, et plus particulièrement avec la [reconnaissance d'entités nommées](https://fr.wikipedia.org/wiki/Reconnaissance_d%27entit%C3%A9s_nomm%C3%A9es)
+(REN). La REN est souvent résolue à l'aide de méthodes d'apprentissage supervisé séquentiel. Cette démo utilise une approche de ce type : nous avons entraîné un modèle en utilisant des décisions de justice annotées indirectement. Une fois entraîné, le modèle est
+ normalement capable de repérer automatiquement des entités dans de nouveaux documents (ici noms, prénoms, adresses). 
 
  
 """)
 
 
 UTILISER = dcc.Markdown("""
-* Vous pouvez uploader un document et ensuite regarder les entités repérées dans le texte ainsi que le texte pseudonymisé
+* Vous pouvez télécharger un document et voir ensuite les entités repérées dans le texte ainsi que le texte pseudonymisé
 sur l'onglet **Pseudonymisez un document**.
-* La quantité des documents annotés à la main a une certaine relation avec la qualité de l'annotation automatique. Trouvez un
+* La quantité de documents annotés à la main a une certaine relation avec la qualité de l'annotation automatique. Trouvez un
 exemple de cette relation dans l'onglet **Volume des données annotées**.
 * Dans **Stats** nous affichons les statistiques d'utilisation de notre API de pseudonymisation.
 """)
@@ -51,7 +49,7 @@ tab_about_content = dbc.Tab(
     label='À propos',
     tab_id="tab-about",
         children=html.Div(className='page', children=[
-        html.H4(className='what-is', children='La pseudonymisation en quelque mots'),
+        html.H4(className='what-is', children='La pseudonymisation en quelques mots'),
         html.P(QUOI),
         html.H4(className='what-is', children='Pourquoi un outil de pseudonymisation ?'),
         html.P(POURQUOI),
